44b01ddec1c3a861db52a6ffc66081f91adb5102
Nikita Mikhailovskiy <nmihaylovskiy@gmail.com>
2021-03-08 01:58:28 +0300

    Digital outputs test script added
    Signed-off-by: Nikita Mikhailovskiy <nmihaylovskiy@gmail.com>

--- src/toggle_by_id.cpp
+++ src/toggle_by_id.cpp
@@ -3,44 +3,54 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "simple_uvc_control_client");
-  if (argc != 2)
-  {
-    ROS_INFO("Usage: toggle_by_id NUM[1..4]");
-    return 1;
-  }
-
+  ros::init(argc, argv, "toggle_relay");
   ros::NodeHandle n;
-  ros::ServiceClient client = n.serviceClient<ultrabot_stm::uvc>("toggle_by_id");
-  ultrabot_stm::uvc srv;
-  switch (atoll(argv[1]))
-  {
-  case 1:
-      srv.request.left1 = !srv.response.status[0];
-      break;
-  case 2:
-      srv.request.left2 = !srv.response.status[1];
-      break;
-  case 3:
-      srv.request.right1 = !srv.response.status[2];
-      break;
-  case 4:
-      srv.request.right2 = !srv.response.status[3];
-      break;  
-  case 5:
-      srv.request.fans = !srv.response.status[4];
-      break;    
-  default:
-      break;
-  }
-  if (client.call(srv))
-  {
-    ROS_INFO("Service called successfully");
-  }
-  else
-  {
-    ROS_ERROR("Failed to call service uvc control");
-    return 1;
+  ros::ServiceClient client = n.serviceClient<ultrabot_stm::uvc>("uvc");
+  ultrabot_stm::uvc::Request req;
+  ultrabot_stm::uvc::Response resp;
+
+  bool statuses[5] = {false,false,false,false,false};
+  int c=0;
+  while(1){
+    switch (c)
+    {
+    case 0:
+        req.left1 = !statuses[0];
+        break;
+    case 1:
+        req.left2 = !statuses[1];
+        break;
+    case 2:
+        req.right1 = !statuses[2];
+        break;
+    case 3:
+        req.right2 = !statuses[3];
+        break;  
+    case 4:
+        req.fans = !statuses[4];
+        break;    
+    default:
+        break;
+    }
+    c++;
+    if(c>4) c = 0;
+    ros::service::waitForService("uvc", 5);
+    bool success = client.call(req,resp);
+    if (success)
+    {
+      for (int i = 0; i<5; i++){
+        statuses[i]= resp.status[i];
+        if(resp.timeout[i] != 0) ROS_INFO("Timeout: [%d]", resp.timeout[i]);
+      }
+      
+      ROS_INFO("Service called successfully");
+    }
+    else
+    {
+      ROS_ERROR("Failed to call service uvc");
+      return 1;
+    }
+    ros::Duration(3).sleep();
   }
 
   return 0;
